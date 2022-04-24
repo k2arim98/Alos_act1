1ad7414f119f856146399d7ce181ace5dfc9b047
Khelifa Abdelkarim <86931754+k2arim98@users.noreply.github.com>
2022-04-24 20:08:42 +0100

    Delete TP3 directory

--- TP3/src/utils/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    jwtsecret: "adir1g22oi8jd6fger411lguh84sduqp848iur"
-}
--- TP3/src/utils/errorHandlers.js
+++
@@ -1,7 +0,0 @@
-export default {
-    notFound: (req, res, next) => {
-        res.status(404).json({
-            error: 'Resource Not Found'
-        })
-    }
-}
